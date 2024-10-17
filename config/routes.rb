e1b94c710ecbce773248cbedc4bdb0f6ff17bc59
shi-n-ya411 <180539615+shi-n-ya411@users.noreply.github.com>
2024-10-17 14:37:10 +0000

    [update]ビューファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+# constraints host: 'old.example.net' do
+#   get '/(*path)', to: redirect { |path_params,| "https://new.example.com/#{path_params[:path]}" }
+# end
+  # get 'bookers_taka.com', to: 'homes#top'
+
+  get 'top' => 'homes#top'
   get 'books/index'
   get 'books/show'
   get 'books/edit'
-  get 'homes/top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

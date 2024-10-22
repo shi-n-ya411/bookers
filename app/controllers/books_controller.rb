2d80e78e5be8e0a6969372534ec59963335a60da
shi-n-ya411 <180539615+shi-n-ya411@users.noreply.github.com>
2024-10-22 11:26:35 +0000

    [Update] バリエーション昨日

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,19 @@
 class BooksController < ApplicationController
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to  book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
-    # @books = Book.all
+    @books = Book.all
     @book = Book.new
-    @books = Book.all.order(created_at: :asc)
+    # @books = Book.all.order(created_at: :asc)
   end
 
   def show
@@ -19,11 +24,21 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  # def update
+  #   book = Book.find(params[:id])
+  #   book.update(book_params)
+  #   redirect_to book_path(book.id)
+  # end
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
+
 
   def destroy
     book = Book.find(params[:id])

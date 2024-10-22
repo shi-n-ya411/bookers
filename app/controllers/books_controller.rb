1057671caf1899b02dec00873746aafb92670fe3
shi-n-ya411 <180539615+shi-n-ya411@users.noreply.github.com>
2024-10-22 10:01:19 +0000

    [Update] pathの記述を書き換え

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,13 +22,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)  
+    redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to books_path
   end
 
   private
